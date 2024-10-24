dc5c43bfbca6b0f9a887f9d8fd8b6772fed8b95f
hizounya <hizounyamaycinto@gmail.com>
2024-10-24 10:04:10 +0700

    membuat input review dan ubah field database detail_makanan

--- detail_makanan/views.py
+++ detail_makanan/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Food, FoodReviews
+from django.http import JsonResponse
+from django.shortcuts import render,get_object_or_404,redirect
+from .models import Food
+from .forms import FoodReviewForm
 
 def show_food_details(request, name):
     food = get_object_or_404(Food, name=name)
     food_reviews = food.foodreviews_set.all()
+
+    if request.method == 'POST':
+        form = FoodReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            food_review = form.save(commit=False)
+            food_review.user = request.user
+            food_review.food = food
+            food_review.save()
+
+            return redirect(f'/food-details/{food.name}')
+    else:
+        form = FoodReviewForm()
+
     context = {
         'food': food,
-        'reviews': food_reviews
+        'reviews': food_reviews,
+        'form': form
     }
-    return render(request, 'food_details.html', context)
+    
+    return render(request, 'food-details.html', context)
