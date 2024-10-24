7690b0f415346313ef5271435395d3b86e151f37
hizounya <hizounyamaycinto@gmail.com>
2024-10-24 11:46:56 +0700

    inshaallah selesai

--- detail_makanan/models.py
+++ detail_makanan/models.py
@@ -1,6 +1,12 @@
-import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+# buat upload foodreview
+def upload_to(instance, filename):
+    current_time = now().strftime('%Y%m%d%H%M%S')
+    return os.path.join('detail_makanan', 'reviews', f'{instance.food.name}', f'{current_time}_{filename}')
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
@@ -16,4 +22,6 @@ class FoodReviews(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     review = models.CharField(max_length=500)
-    image_url = models.ImageField(upload_to='review_images/', blank=True, null=True)
+    image_url = models.ImageField(upload_to=upload_to, blank=True, null=True)
+
+
