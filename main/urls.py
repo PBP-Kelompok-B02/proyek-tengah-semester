1dbb72e6bd20f316b1c6c3864336bf158e2b065d
lisamrgrth <lisatobing1610@gmail.com>
2024-10-25 16:06:12 +0700

    Forum update

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_main, show_json, register, login_user, logout_user, show_profile, show_bookmarks, show_forum, show_food_details
+from .views import delete_forum, delete_reply, show_main, show_json, register, login_user, logout_user, show_profile, show_bookmarks, show_forum, show_food_details, create_forum, submit_forum, reply_forum
 
 app_name = 'main'
 
@@ -11,7 +11,14 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('profile/', show_profile, name='profile'),
     path('bookmark/', show_bookmarks, name='bookmark'),
-    path('forum/', show_forum, name='forum'),
     path('food-details/<name>', show_food_details, name='food_details'), 
-    path('')
+    
+    path('forum/<int:forum_id>/delete/', delete_forum, name='delete_forum'),
+    path('reply/<int:reply_id>/delete/', delete_reply, name='delete_reply'), 
+
+    path('forum/', show_forum, name='forum'),
+    path('forum/create_forum/', create_forum, name='create_forum'),
+    path('forum/submit_forum/', submit_forum, name='submit_forum'),
+    path('forum/<int:forum_id>/delete/', delete_forum, name='delete_forum'),
+    path('forum/<int:forum_id>/reply/', reply_forum, name='reply_forum'),
 ]
