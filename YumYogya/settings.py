f6f2adbe87b6c1d9dd1fb93f25ec72152de86699
lisamrgrth <lisatobing1610@gmail.com>
2024-10-26 15:46:48 +0700

    Bener pleasee

--- YumYogya/settings.py
+++ YumYogya/settings.py
@@ -141,4 +141,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://lisa-margaretha-yumyogya.pbp.cs.ui.ac.id", "https://lisa-margaretha-yumyogya.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://lisa-margaretha-yumyogya.pbp.cs.ui.ac.id", "https://lisa-margaretha-yumyogya.pbp.cs.ui.ac.id"] 
