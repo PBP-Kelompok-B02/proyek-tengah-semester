471b0e275c3f8ada35be7a3394560b31038de805
lisamrgrth <lisatobing1610@gmail.com>
2024-12-09 08:06:14 +0700

    Add mobile forum submission and reply endpoints

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import show_forum, create_forum, submit_forum, delete_forum, reply_forum, delete_reply, show_json
+from .views import show_forum, create_forum, submit_forum, delete_forum, reply_forum, delete_reply, show_json, submit_forum_mobile, reply_forum_mobile
 
 app_name = 'forum'
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('<int:forum_id>/delete/', delete_forum, name='delete_forum'),  
     path('<int:forum_id>/reply/', reply_forum, name='reply_forum'),  
     path('<int:reply_id>/delete-reply/', delete_reply, name='delete_reply'),
+    path('submit-forum/mobile/', submit_forum_mobile, name='submit_forum_mobile'),  
+    path('<int:forum_id>/reply/mobile/', reply_forum_mobile, name='reply_forum_mobile'),  
 ]
